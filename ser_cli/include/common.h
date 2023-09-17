efea4e9dd000ba5a864e9be8d622a8c6b73d586c
iranjan09 <140534526+iranjan09@users.noreply.github.com>
2023-09-17 18:03:30 -0500

    Update common.h

--- ser_cli/include/common.h
+++ ser_cli/include/common.h
@@ -1,16 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef COMMON_INCLUDED
+#define COMMON_INCLUDED
+
+/#include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <signal.h>
+#include <wait.h>
 
-#define BUFFER_SIZE 1024
+#define QUEUE_SIZE 50       // Max Queue Size
+#define BUFFER_SIZE 5      // Max Buffer Size
 
-int writen(int fd, char *buf, int bufSize);
-int readLine (int iSocket_fd, char *pcBuffer, int iBufferSize);
+// Function decleration
+int server_sock_init(struct sockaddr_in *sockaddr, char *ip_addr, int port);
+ssize_t readLine(int fd, void *vptr, size_t maxlen); 
+ssize_t writen(int fd, void *buf_ptr, size_t bufSize);
+void zombie_handler_func(int signum);
+void read_write(int connect_fd);
 int send_data(int sockfd, char *data);
-int receive_data(int sockfd, char *buffer, int buf_size) 
+int receive_data(int sockfd, char *buffer, int buf_size);
+
+#endif // COMMON_INCLUDED
