16fab70cf113bd3f552e2529d2ca02f992729627
iranjan09 <140534526+iranjan09@users.noreply.github.com>
2023-09-17 18:01:12 -0500

    Update common.c

--- ser_cli/src/common.c
+++ ser_cli/src/common.c
@@ -6,7 +6,7 @@ static int read_cnt;
 static char *read_ptr;
 static char read_buf[BUFFER_SIZE];
 
-//Read functions from W. Richard Stevens, Bill Fenner, and Andrew M. Rudoff, Unix Network
+//Read/Write functions from W. Richard Stevens, Bill Fenner, and Andrew M. Rudoff, Unix Network
 //Programming, Volume 1, The Sockets Networking API, 3rd Edition, Addison- Wesley,
 //2004, Chapter 3.
 
@@ -47,23 +47,26 @@ ssize_t readLine(int fd, void *vptr, size_t maxlen) {
     return (n);
 }
 
-int writen(int fd, char *buf, int bufSize) {
+ssize_t writen(int fd, void *buf_ptr, size_t bufSize) {
   
-  int nleft = bufSize; 
-  int nwritten;
+  ssize_t bytes_left = bufSize; 
+  ssize_t bytes_written;
+  char *ptr;
+  
+  ptr = buf_ptr;
 
-  while (nleft > 0) {
-    if ((nwritten = write(fd, buf, nleft)) < 0) {
-      if (nwritten < 0 && errno == EINTR) {
-        nwritten = 0; 
+  while (bytes_left > 0) {
+    if ((bytes_written = write(fd, ptr, bytes_left)) <= 0) {
+      if (bytes_written < 0 && errno == EINTR) {
+        bytes_written = 0; 
       }
       else {
         return -1; 
       }
     }
 
-    nleft -= nwritten;
-    buf += nwritten;
+    bytes_left -= bytes_written;
+    ptr += bytes_written;
   }
 
   return bufSize;
@@ -99,8 +102,6 @@ int receive_data(int sockfd, char *buffer, int buf_size) {
 	if (ret < 0) {
 		perror("Receive failed");
 	}
-
 	return ret;
 
 }
-
